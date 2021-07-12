99841f625e23f8a686544d705bbbe8a7a0622972
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-07-12 19:19:31 +0800

    支持IE11等旧浏览器

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    legacy({
+      targets: ["ie >= 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
   server: { proxy: { "/api": "http://127.0.0.1:3001/" } },
 });
