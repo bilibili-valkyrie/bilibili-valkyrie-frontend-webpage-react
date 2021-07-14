ce9f8498e2848f28da81e2e488b0cd0531dc3d50
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-07-14 18:30:31 +0800

    add service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// eslint-disable-next-line import/no-unresolved
+import { registerSW } from "virtual:pwa-register";
 import App from "./App";
 
+registerSW({
+  onOfflineReady() {
+    // show a ready to work offline to user
+  },
+});
 ReactDOM.render(<App />, document.getElementById("root"));
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import legacy from "@vitejs/plugin-legacy";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,15 @@ export default defineConfig({
     legacy({
       targets: ["ie >= 11"],
       additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        // content of manifest
+      },
+      workbox: {
+        // workbox options for generateSW
+      },
     }),
   ],
   server: { proxy: { "/api": { target: "http://127.0.0.1:3001/", ws: true } } },
